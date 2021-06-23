04365aaa7a48dcb6e1d3a3eeeda9ab9c9618c10d
shun <s.1993.1029@gmail.com>
2021-06-23 22:46:16 +0900

    テスト

--- src/PostItem.js
+++ src/PostItem.js
@@ -4,7 +4,7 @@ const PostItem = (props) =>{
     const {title, date,url} = props.post;
     return(
         <div className={styles.container}>
-            <a href="{url}">
+            <a href={url}>
                 <span className={styles.date}>{date}</span>
                 <span className={styles.title}>{title}</span>
             </a>
